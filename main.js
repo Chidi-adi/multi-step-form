51781a639f35639d616fc9a51a52811953e0cbb1
Chidi-adi <nwosuchidiadi@gmail.com>
2023-01-14 22:02:29 +0100

    modified css styling, removed tailwindcss and added plans and add-ons form

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const menuBtn = document.querySelector("#menuBtn");
-menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
+const nameInput = document.getElementById("nameInput");
+console.log(nameInput.value);
+nameInput.addEventListener("input", (e) => {
+  if (e.target.value.length < 3) {
+    return;
+  } else if (e.target.value.length >= 3) {
+    e.target.style.border = "solid 1px red";
+  }
 });
